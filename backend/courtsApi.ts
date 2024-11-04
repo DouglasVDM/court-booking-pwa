f3f40b5fea284b4ca45d6239bf6a0cdd08688a96
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-04 20:58:25 +0200

    Remove / move files

--- backend/courtsApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE = 'http://localhost:5000';
-
-export const fetchCourts = () => axios.get(`${API_BASE}/courts`);
-export const addCourt = (data: object) => axios.post(`${API_BASE}/courts`, data);
-export const updateCourt = (id: number, data: object) => axios.put(`${API_BASE}/courts/${id}`, data);
-export const deleteCourt = (id: number) => axios.delete(`${API_BASE}/courts/${id}`);
--- backend/offeringsApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE = 'http://localhost:5000';
-
-export const fetchOfferings = () => axios.get(`${API_BASE}/offerings`);
-export const addOffering = (data: object) => axios.post(`${API_BASE}/offerings`, data);
-export const updateOffering = (id: number, data: object) => axios.put(`${API_BASE}/offerings/${id}`, data);
-export const deleteOffering = (id: number) => axios.delete(`${API_BASE}/offerings/${id}`);
