a5dcf7dc5eebe4930a359d6fc48c66edb9a30867
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-06 21:04:31 +0200

    Remove boilerplate code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+
+const root = createRoot(document.getElementById('root'));
+const YOUR_AUTH0_DOMAIN=import.meta.env.VITE_YOUR_AUTH0_DOMAIN
+const YOUR_AUTH0_CLIENT_ID=import.meta.env.VITE_YOUR_AUTH0_CLIENT_ID
+
+
+root.render(
+<Auth0Provider
+     domain={YOUR_AUTH0_DOMAIN}
+     clientId={YOUR_AUTH0_CLIENT_ID}
+     authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>,
-)
+  </Auth0Provider>,
+);
