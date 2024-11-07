8e5184b922ea697a2f65a99fbef3e59f48c4db0e
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-07 21:56:05 +0200

    Create Profile.tsx

---
+++ frontend/src/components/Profile.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "./LogoutButton";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  const navigate = useNavigate();
+
+  console.log(user)
+
+  if (!isAuthenticated) {
+    navigate("/");
+  }
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2 id="user-name">{user.name}</h2>
+        <p>{user.email}</p>
+        <LogoutButton />
+      </div>
+    )
+  );
+};
+
+export default Profile;
