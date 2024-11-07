d61502eef48808f6da346b8d43dbbc151a1a60cb
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-07 21:55:58 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import Profile from "./components/Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <LoginButton />
-      <LogoutButton />
-    </div>
-  );
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+  return isAuthenticated ? <Profile /> : <LoginButton />;
 }
 
 export default App;
