07654a6caf939a679079909a40989445224eb5b7
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-09 07:34:47 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App.tsx";
 
-
-const root = createRoot(document.getElementById('root'));
-const YOUR_AUTH0_DOMAIN=import.meta.env.VITE_YOUR_AUTH0_DOMAIN
-const YOUR_AUTH0_CLIENT_ID=import.meta.env.VITE_YOUR_AUTH0_CLIENT_ID
-
+const root = createRoot(document.getElementById("root"));
+const YOUR_AUTH0_DOMAIN = import.meta.env.VITE_YOUR_AUTH0_DOMAIN;
+const YOUR_AUTH0_CLIENT_ID = import.meta.env.VITE_YOUR_AUTH0_CLIENT_ID;
 
 root.render(
-<Auth0Provider
-     domain={YOUR_AUTH0_DOMAIN}
-     clientId={YOUR_AUTH0_CLIENT_ID}
-     authorizationParams={{
-      redirect_uri: window.location.origin
+  <Auth0Provider
+    domain={YOUR_AUTH0_DOMAIN}
+    clientId={YOUR_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
     }}
   >
     <App />
-  </Auth0Provider>,
+  </Auth0Provider>
 );
