885ffa00caad50cdeff909b1f1b7faed35a88870
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-10 13:37:03 +0200

    Update Home.tsx

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import Profile from "./Profile";
+import LoginButton from "./LoginButton";
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+function Home() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+  return isAuthenticated ? <Profile /> : <LoginButton />;
 }
 
-export default Home
+export default Home;
