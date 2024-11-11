0d98a8209bfe8e2c0857979960f1d6d288133977
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-12 00:13:28 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import LoginButton from "./components/LoginButton";
-import { useAuth0 } from "@auth0/auth0-react";
-import Profile from "./components/Profile";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import Profile from "./components/pages/Profile";
+import { Navbar } from "./components/pages/Navbar";
+import { Error } from "./components/pages/Error";
+import LandingPage from "./components/pages/LandingPage";
+import { Loading } from "./components/pages/Loading";
 
 function App() {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { error, isLoading } = useAuth0();
+  const ProtectedProfile = withAuthenticationRequired(Profile);
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <Loading />;
   }
-  return isAuthenticated ? <Profile /> : <LoginButton />;
+  return (
+    <>
+      <Navbar />
+      {error && <Error message={error.message} />}
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/profile" element={<ProtectedProfile />} />
+      </Routes>
+    </>
+  );
 }
-
 export default App;
