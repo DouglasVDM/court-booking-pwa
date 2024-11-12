3be5b108380818e14846405f04788989e9fef69e
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-12 22:43:26 +0200

    Install helmet and morgan

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import bodyParser, { BodyParser } from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -15,13 +17,12 @@ const visitorRoutes = require("./routes/visitorRoutes");
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
+app.use(helmet()); // Using Helmet to secure headers
+app.use(morgan("dev")); // 'dev' is a predefined format that gives concise colored output
 
 // Routes
 app.use("/members", memberRoutes);
 app.use("/visitors", visitorRoutes);
-
-
-// Test route to confirm database connection
 
 // Test route to confirm server connection
 app.get("/", (req, res) => {
