f120713bffd2b7d66e7ae33c1ce5767428bede3d
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-17 01:57:27 +0200

    Add courts route link

--- frontend/src/components/pages/Navbar.tsx
+++ frontend/src/components/pages/Navbar.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useLocation, Link } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import LogoutButton from './LogoutButton';
-import LoginButton from './LoginButton'
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "./LogoutButton";
+import LoginButton from "./LoginButton";
 
 export function Navbar() {
-  const { isAuthenticated, user} = useAuth0<{
+  const { isAuthenticated, user } = useAuth0<{
     name: string;
   }>();
   const { pathname } = useLocation();
@@ -16,28 +16,37 @@ export function Navbar() {
         <div className="navbar-nav">
           <Link
             to="/"
-            className={`nav-item nav-link${pathname === '/home' ? ' active' : ''}`}
+            className={`nav-item nav-link${
+              pathname === "/home" ? " active" : ""
+            }`}
           >
             Home
           </Link>
           <Link
             to="/profile"
             className={`nav-item nav-link${
-              pathname === '/profile' ? ' active' : ''
+              pathname === "/profile" ? " active" : ""
             }`}
           >
             Profile
+          </Link>
+          <Link
+            to="/courts"
+            className={`nav-item nav-link${
+              pathname === "/courts" ? " active" : ""
+            }`}
+          >
+            Courts
           </Link>
         </div>
       </div>
 
       {isAuthenticated ? (
         <div>
-          <span id="hello">Hello, {user?.name}!</span>{' '}
-          <LogoutButton/>
+          <span id="hello">Hello, {user?.name}!</span> <LogoutButton />
         </div>
       ) : (
-        <LoginButton/>
+        <LoginButton />
       )}
     </nav>
   );
