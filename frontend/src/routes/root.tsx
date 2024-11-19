da938a0fa57dd30b03de1bd15d82bb725f87ec1d
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-19 19:41:45 +0200

    Create root.tsx

---
+++ frontend/src/routes/root.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import Profile from "../components/Profile";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import NotFound from "../components/NotFound";
+import Home from "../components/Home";
+import LoginButton from "../components/LoginButton";
+import MembersList from "../components/members/MembersList";
+import CourtsList from "../components/courts/CourtsList";
+import DashboardPage from "../pages/DashboardPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginButton />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardPage />,
+  },
+  {
+    path: "/courts",
+    element: <CourtsList />,
+  },
+  {
+    path: "/members",
+    element: <MembersList />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default function root() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+  <>
+    <div className="app" id="detail">
+      <Navbar />
+      <RouterProvider router={router} />
+      <Footer />
+      <Outlet />
+    </div>
+  </>;
+}
