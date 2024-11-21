43a29e2463e196376e7f8e55fa21aafea039ef1b
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-21 22:15:17 +0200

    Add date picker component

--- frontend/src/pages/BookingPage.tsx
+++ frontend/src/pages/BookingPage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import CourtsPage from "./CourtsPage";
 import BookingTypesPage from "./BookingTypesPage";
-
+import DatePicker from "../components/DatePicker"
 const BookingPage = ({ courts, bookingTypes }) => {
   return (
     <>
     <h1>Booking Page</h1>
       <CourtsPage courts={courts} />
       <BookingTypesPage bookingTypes={bookingTypes} />
+      <DatePicker/>
     </>
   );
 };
