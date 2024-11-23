dd831db7d557f98b3219f786d662a8c950fb6439
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-23 12:18:21 +0200

    Create timeSlotRoutes.ts

---
+++ backend/routes/timeSlotRoutes.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from "express";
+const { pool } = require("../database_config/pool");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const query = "SELECT * FROM time_slot";
+    const { rows } = await pool.query(query);
+    res.status(200).json(rows);
+    console.log("Success: Get all time slots");
+  } catch (err) {
+    console.error((err as Error).message);
+    res.status(500).send("Could not get time slots");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params; // Extract court from the URL parameter
+    const query = "SELECT * FROM time_slot WHERE id = $1";
+    const { rows } = await pool.query(query, [id]);
+
+    if (rows.length === 0) {
+      return res.status(404).send(`time slot id ${id} not found`);
+    }
+
+    res.status(200).json(rows[0]);
+    console.log(`Success: Get time slot by id ${id}`);
+  } catch (err) {
+    console.error((err as Error).message);
+    res.status(500).send("Could not get time slot by id");
+  }
+});
+
+module.exports = router;
