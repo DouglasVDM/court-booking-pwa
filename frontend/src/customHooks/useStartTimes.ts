2ec91cf5a04e1267388cb9d973c03ff3a502ad09
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-28 23:31:40 +0200

    Create useStartTimes.ts

---
+++ frontend/src/customHooks/useStartTimes.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useStartTimes = (apiEndpointPrefix) => {
+  const [startTimes, setStartTimes] = useState([]);
+
+  useEffect(() => {
+    const fetchstartTimes = async () => {
+      try {
+        const startTimesResponse = await axios.get(`${apiEndpointPrefix}/startTimes`);
+        setStartTimes(startTimesResponse.data);
+        console.log(startTimesResponse.data)
+      } catch (err) {
+        console.error("Error fetching startTimes:", err.message);
+      }
+    };
+
+    fetchstartTimes();
+  }, [apiEndpointPrefix]);
+
+  return { startTimes, setStartTimes };
+};
+
+export default useStartTimes;
