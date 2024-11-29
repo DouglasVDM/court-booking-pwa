3c8c7eb8b84f147686db81422bef0ef5073d0dcc
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-11-29 23:03:43 +0200

    Add styling to move form label to the left

--- frontend/src/pages/TimeSelector.tsx
+++ frontend/src/pages/TimeSelector.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 
 interface StartTime {
@@ -13,42 +14,48 @@ const TimeSelector = ({ startTimes, endTimes }) => {
   const handleSelectedStartTime = (event) => {
     const selectedStartTimeSlot = event.target.value;
     setSelectedStartTime(selectedStartTimeSlot);
-    console.log("start_time",selectedStartTimeSlot);
+    console.log("start_time", selectedStartTimeSlot);
   };
 
   const handleSelectedEndTime = (event) => {
     const selectedEndTimeSlot = event.target.value;
     setSelectedEndTime(selectedEndTimeSlot);
-    console.log("end_time",selectedEndTimeSlot);
+    console.log("end_time", selectedEndTimeSlot);
   };
 
   // onClick={handleSelectedStartTime(selectedStartTime)}
 
   return (
-    <>
-      <Form.Group>
-        <Form.Label>Select a start time</Form.Label>
-        <Form.Select size="lg" onChange={handleSelectedStartTime}>
-          <option aria-label="select a start time">Select a start time</option>
-          {startTimes.map(({ start_time_id, start_time }) => (
-            <option key={start_time_id} value={start_time}>
-              {start_time}
+    <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridStartTime">
+          <Form.Label className="text-start w-100">Select a start time</Form.Label>
+          <Form.Select size="lg" onChange={handleSelectedStartTime}>
+            <option aria-label="select a start time">
+              Select a start time
             </option>
-          ))}
-        </Form.Select>
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Select a end time</Form.Label>
-        <Form.Select size="lg" onChange={handleSelectedEndTime}>
-          <option aria-label="select a end time">Select a end time</option>
-          {endTimes.map(({ end_time_id, end_time }) => (
-            <option key={end_time_id} value={end_time}>
-              {end_time}
+            {startTimes.map(({ start_time_id, start_time }) => (
+              <option key={start_time_id} value={start_time}>
+                {start_time}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridStartTime">
+          <Form.Label className="text-start w-100">Select a end time</Form.Label>
+          <Form.Select size="lg" onChange={handleSelectedEndTime}>
+            <option aria-label="select a end time">
+              Select a end time
             </option>
-          ))}
-        </Form.Select>
-      </Form.Group>
-    </>
+            {endTimes.map(({ end_time_id, end_time }) => (
+              <option key={end_time_id} value={end_time}>
+                {end_time}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+      </Row>
+    </Form>
   );
 };
 
