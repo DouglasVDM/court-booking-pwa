fdf2db1a6aaf2f024ea04ef7da4544e11ac99241
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-12-04 06:21:24 +0200

    Change to select drop down
    It was radios

--- frontend/src/pages/BookingTypesPage.tsx
+++ frontend/src/pages/BookingTypesPage.tsx
@@ -5,65 +5,35 @@ import Row from "react-bootstrap/Row";
 import PropTypes from "prop-types";
 
 const BookingTypesPage = ({ bookingTypes }) => {
-  
-  const [selectedBookingType, setSelectedBookingType] = useState(null);
+  const [selectedBookingType, setSelectedBookingType] = useState([]);
 
-  const handleClick = (event) => {
-    const selectedBookingType = bookingTypes.find(
-      (bookingType) => bookingType.booking_type_id === parseInt(event.target.value)
-    );
-    console.log("selectedBookingType", selectedBookingType);
-    console.log("event.target.value", event.target.value);
-    console.log("bookingTypes", bookingTypes);
-    setSelectedBookingType(selectedBookingType);
+  const handleSelectedBookingType = (event) => {
+    const bookingType = event.target.value;
+    setSelectedBookingType(bookingType);
+    console.log("bookingType", selectedBookingType);
   };
 
   return (
-    <>
-      <Form>
-        
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label as="legend" className="text-start">
-            Please select your booking type
+    <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridBooking">
+          <Form.Label className="text-start w-100">
+            Select a booking type
           </Form.Label>
-          {bookingTypes.map(({ booking_type_id, booking_type_name, has_lights }) => (
-            <Col key={booking_type_id} sm={15}>
-              <Form.Label>
-                <p>{has_lights}</p>
-              </Form.Label>
-              <Form.Check
-                onClick={handleClick}
-                type="radio"
-                label={
-                  <span>
-                    {booking_type_name}
-                    {has_lights && (
-                      <span style={{ color: "green", marginLeft: "10px" }}>
-                        (Has Lights)
-                      </span>
-                    )}
-                  </span>
-                }
-                name="selectedBookingType"
-                id={booking_type_id}
-                value={booking_type_id}
-              />
-            </Col>
-          ))}
+          <Form.Select size="lg" onChange={handleSelectedBookingType}>
+            <option aria-label="select a bookingType">
+              Select a booking type
+            </option>
+            {bookingTypes.map(({ booking_type_id, booking_type_name }) => (
+              <option key={booking_type_id} value={booking_type_name}>
+                {booking_type_name}
+              </option>
+            ))}
+          </Form.Select>
         </Form.Group>
-      </Form>
-    </>
+      </Row>
+    </Form>
   );
-};
-
-BookingTypesPage.propTypes = {
-  bookingTypes: PropTypes.arrayOf(
-    PropTypes.shape({
-      booking_type_id: PropTypes.number.isRequired,
-      booking_type_name: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  //   onbookingTypeselected: PropTypes.func.isRequired,
 };
 
 export default BookingTypesPage;
