1fbd08c103d6c0a37e045878e3f0e72d1c5f487c
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-12-04 06:53:15 +0200

    Move not found page and home route
    Display only if there's an error after page loader
    Display booking page after login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
-
-
 // Components
 import PageLoader from "./components/PageLoader";
 import PageLayout from "./components/PageLayout";
@@ -18,31 +16,24 @@ import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   const { error, isLoading } = useAuth0();
- 
+
   const ProtectedProfile = withAuthenticationRequired(Profile);
   const ProtectedBooking = withAuthenticationRequired(BookingPage);
   const ProtectedDashboard = withAuthenticationRequired(DashboardPage);
 
   if (isLoading) {
     return <PageLoader />;
+  } else {
+    error && <NotFoundPage message={error.message} />;
   }
   return (
     <>
       <PageLayout>
-        {" "}
-        {error && <NotFoundPage message={error.message} />}
         <Routes>
-          <Route path="/" element={<HomePage />} />
-
+          <Route path="/" element={<ProtectedBooking />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/dashboard" element={<ProtectedDashboard />} />
           <Route path="/profile" element={<ProtectedProfile />} />
-          <Route
-            path="/bookings"
-            element={
-              <ProtectedBooking
-              />
-            }
-          />
         </Routes>
       </PageLayout>
     </>
