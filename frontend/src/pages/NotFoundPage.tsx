887347a674113f6d3a0b07816d40a04d3cf158e0
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-12-04 06:48:59 +0200

    Move not found page
    Display only if there's an error after page loader

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -2,13 +2,11 @@ import PageLayout from "../components/PageLayout";
 
 const NotFoundPage = () => {
   return (
-    <PageLayout>
-      <div className="content-layout">
-        <h1 id="page-title" className="content__title">
-          Not Found
-        </h1>
-      </div>
-    </PageLayout>
+    <div className="content-layout">
+      <h1 id="page-title" className="content__title">
+        Not Found
+      </h1>
+    </div>
   );
 };
 
