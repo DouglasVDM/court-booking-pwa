0234b6df35e15de574f6446c390e35d67c2ea1f8
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-12-09 22:02:45 +0200

    Fetch bookings

--- frontend/src/pages/BookingPage.tsx
+++ frontend/src/pages/BookingPage.tsx
@@ -3,6 +3,7 @@ import CourtsPage from "./CourtsPage";
 import BookingTypesPage from "./BookingTypesPage";
 import DatePickerPage from "../components/DatePickerPage";
 import StartTimeSelector from "./TimeSelector";
+import BookingsList from "./BookingList";
 
 const apiEndpointPrefix = import.meta.env.VITE_API_ENDPOINT;
 
@@ -11,16 +12,19 @@ import useCourts from "../customHooks/useCourts";
 import useBookingTypes from "../customHooks/useBookingTypes";
 import useStartTimes from "../customHooks/useStartTimes";
 import useEndTimes from "../customHooks/useEndTimes";
+import useFetchBookings from "../customHooks/useFetchBookings";
 
 const BookingPage = () => {
   const { courts } = useCourts(apiEndpointPrefix);
   const { bookingTypes } = useBookingTypes(apiEndpointPrefix);
   const { startTimes } = useStartTimes(apiEndpointPrefix);
   const { endTimes } = useEndTimes(apiEndpointPrefix);
+  const { bookings } = useFetchBookings(apiEndpointPrefix);
 
   return (
     <>
       <h1>Booking Page</h1>
+      <BookingsList bookings={bookings}/>
       <DatePickerPage />
       <CourtsPage courts={courts} />
       <BookingTypesPage bookingTypes={bookingTypes} />
