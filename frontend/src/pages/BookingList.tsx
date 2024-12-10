efd9305b5df0e66f686375a979f8fc2a924ef579
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-12-10 07:05:13 +0200

    Change booking list to display cards

--- frontend/src/pages/BookingList.tsx
+++ frontend/src/pages/BookingList.tsx
@@ -1,22 +1,32 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import { CardTitle, Row } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
 
 const BookingsList = ({ bookings }) => {
   console.log(bookings);
 
   return (
-    <div>
-      <h3>Bookings</h3>
-      <ul>
-        {bookings.map((booking) => (
-          <li key={booking.booking_id}>
-            {booking.first_name} {booking.surname}_booked_{booking.court_name}_for_
-            {booking.booking_type_name}_on_
-            {booking.day_name} {booking.booking_date}_starting at_
-            {booking.start_time}_finishing at_{booking.end_time}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Row>
+      <h1>Bookings</h1>
+      {bookings.map((booking) => (
+        <Col key={booking.booking_id} md={4}>
+          <Card>
+            <CardTitle>
+              {booking.first_name}_{booking.surname}
+            </CardTitle>
+            <Card.Text>
+              <strong>Court:</strong> {booking.court_id} <br />
+              <strong>Type:</strong> {booking.booking_type_name} <br />
+              <strong>Date:</strong>
+              {new Date(booking.booking_date).toLocaleDateString()} <br />
+              <strong>Start:</strong> {booking.start_time} <br />
+              <strong>End:</strong> {booking.end_time}
+            </Card.Text>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 };
 
