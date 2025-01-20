7f8770b85244daff373692ac197c44630dfb6bed
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2025-01-20 22:16:43 +0200

    Added more error handling

--- frontend/src/customHooks/useFetchBookings.ts
+++ frontend/src/customHooks/useFetchBookings.ts
@@ -3,6 +3,8 @@ import axios from "axios";
 
 const useFetchBookings = (apiEndpointPrefix) => {
   const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(true); // Loading state
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchBookings = async () => {
@@ -10,15 +12,21 @@ const useFetchBookings = (apiEndpointPrefix) => {
         const fetchBookingResponse = await axios.get(
           `${apiEndpointPrefix}/bookings`
         );
+        if (!fetchBookingResponse) {
+          throw new Error("Failed to fetch bookings.");
+        }
         setBookings(fetchBookingResponse.data);
       } catch (err) {
         console.error("Error fetching bookings:", err.message);
+        setError(err.message);
+      } finally {
+        setLoading(false);
       }
     };
     fetchBookings();
   }, [apiEndpointPrefix]);
 
-  return { bookings, setBookings };
+  return { bookings, setBookings, loading, error };
 };
 
 export default useFetchBookings;
