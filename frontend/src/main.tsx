bfb226b80460f56513970e6e9f4e51f9865367c2
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2025-01-21 07:35:31 +0200

    Wrap main with new Auth0ProviderWithRedirect
    Import Auth0ProviderWithRedirect

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Auth0ProviderWithRedirect from "./authentication/Auth0ProviderWithRedirect.tsx";
 
 const root = createRoot(document.getElementById("root"));
-const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
-const AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
-const AUTH0_API_AUDIENCE = import.meta.env.VITE_API_AUDIENCE;
 
 root.render(
   <BrowserRouter>
-    <Auth0Provider
-      domain={AUTH0_DOMAIN}
-      clientId={AUTH0_CLIENT_ID}
-      authorizationParams={{ redirect_uri: window.location.origin }}
-    >
+    <Auth0ProviderWithRedirect>
       <div className="app">
         <App />
       </div>
-    </Auth0Provider>
+    </Auth0ProviderWithRedirect>
   </BrowserRouter>
 );
