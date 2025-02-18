7edbeee8485f6ca87543191dccec51f77aa8d433
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2025-02-18 06:02:18 +0200

    Enhanced app stability by wrapping in StrictMode
    Added StrictMode to help detect potential issues during development.
    
    Ensures better debugging and adherence to React best practices.
    
    No impact on production behavior.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
@@ -8,11 +8,13 @@ import Auth0ProviderWithRedirect from "./authentication/Auth0ProviderWithRedirec
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <Auth0ProviderWithRedirect>
-      <div className="app">
-        <App />
-      </div>
-    </Auth0ProviderWithRedirect>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <Auth0ProviderWithRedirect>
+        <div className="app">
+          <App />
+        </div>
+      </Auth0ProviderWithRedirect>
+    </BrowserRouter>
+  </StrictMode>
 );
